c4d33a7518eba62368026c96a6d6ab66e5d0d390
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2017-10-31 13:00:37 -0400

    Bounce the black ball

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -19,10 +19,11 @@ class Sketch : NSObject {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
+        canvas.drawShapesWithBorders = false
         
         // Set starting position
-        x = random(from: 10, toButNotIncluding: 400)
-        y = random(from: 60, toButNotIncluding: 400)
+        x = random(from: 10, toButNotIncluding: 100)
+        y = random(from: 60, toButNotIncluding: 300)
         
         //set the difference
         dx = 1
@@ -34,30 +35,35 @@ class Sketch : NSObject {
     // Runs in a loop, forever, to create the animated effect
     func draw() {
     
+        // Clear the background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
         
         // Change position
         x += dx
         y += dy
 
         
-        if x > 500{
+        if x > 450 {
             dx = -1
         }
         
-        if x < 0{
+        if x < 50 {
             dx = 1
         }
         
-        if y > 500{
+        if y > 450 {
             dy = -1
         }
         
-        if y < 0{
+        if y < 50 {
             dy = 1
         }
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        canvas.fillColor = Color.black
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: y, width: 100, height: 100, borderWidth: x)
         
     }
     
