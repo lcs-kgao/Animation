d40a17def9baab6638565430a2e97df7070ff0e0
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2017-11-02 16:36:50 -0400

    Have done the second circle

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,12 +7,16 @@ class Sketch : NSObject {
     let canvas : Canvas
     
     // Position of circle
-    var x : Int
-    var y : Int
+    var x1 : Int
+    var y1 : Int
+    var x2 : Int
+    var y2 : Int
     
     //change in position
-    var dx : Int
-    var dy : Int
+    var dx1 : Int
+    var dy1 : Int
+    var dx2 : Int
+    var dy2 : Int
     
     // This function runs once
     override init() {
@@ -22,12 +26,16 @@ class Sketch : NSObject {
        // canvas.drawShapesWithBorders = false
         
         // Set starting position
-        x = random(from: 40, toButNotIncluding: 60)
-        y = random(from: 250, toButNotIncluding: 350)
+        x1 = random(from: 100, toButNotIncluding: 150)
+        y1 = random(from: 200, toButNotIncluding: 250)
+        x2 = 200
+        y2 = 250
         
         //set the difference
-        dx = 2
-        dy = 2
+        dx1 = 1
+        dy1 = 1
+        dx2 = 1
+        dy2 = 1
         
         
     }
@@ -40,31 +48,43 @@ class Sketch : NSObject {
         canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
         
         // Change position
-        x += dx
-        y += dy
+        x1 += dx1
+        y1 += dy1
+        x2 += dx2
+        
 
         
-        if x > 450 {
-            dx = -2
+        if x1 > 450 {
+            dx1 = -1
         }
         
-        if x < 50 {
-            dx = 2
+        if x1 < 50 {
+            dx1 = 1
         }
         
-        if y > 450 {
-            dy = -2
+        if y1 > 450 {
+            dy1 = -1
         }
         
-        if y < 50 {
-            dy = 2
+        if y1 < 50 {
+            dy1 = 1
         }
+        
+        if x2 > 500{
+            dx2 = -1
+        }
+        if x2 < 0{
+            dx2 = 1
+        }
+      
         
         // Draw an ellipse in the middle of the canvas
         canvas.fillColor = Color.white
         canvas.borderColor = Color.black
-        canvas.drawEllipse(centreX: x, centreY: y, width: 100, height: 100, borderWidth: 3)
-        canvas.drawEllipse(centreX: x, centreY: y, width: 1, height: 1 , borderWidth: 1)
+        canvas.drawEllipse(centreX: x1, centreY: y1, width: 100, height: 100, borderWidth: 3)
+        canvas.drawEllipse(centreX: x1, centreY: y1, width: 1, height: 1 , borderWidth: 1)
+        canvas.drawEllipse(centreX: x2, centreY: y2, width: 220, height: 220, borderWidth: 3)
+        canvas.drawEllipse(centreX: x2, centreY: y2, width: 1, height: 1, borderWidth: 1)
         
     }
     
