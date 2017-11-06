8f3b89aa20c8a5d3246c8d6c6a6b9795007fb190
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2017-11-06 09:14:48 -0500

    hhahhahhaha

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -36,14 +36,21 @@ class Sketch : NSObject {
         x += 1
         
        // let a : Double = 100
-        
         //y = Int(a*(Double(x)-250)*(Double(x)-250)+500)
-        y = Int(100*sin(0.04188790205*Double(x)))
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: Int(y), width: 5, height: 5)
+        // set the sin equation
+        y = Int(100*sin(0.02631819699*Double(x)))
         
+        //curves run in few times
+        for k in stride(from: 0, to: 500, by: 20){
+            
+        // Draw an ellipse
+            
+            for c in stride(from: 20, to: 360, by: 20){
+            //canvas.fillColor = Color(hue: c, saturation: 100, brightness: 100, alpha: 100)
+        canvas.drawEllipse(centreX: x, centreY: y+k, width: 5, height: 5)
+        }
+        }
     }
-    
 }
 
