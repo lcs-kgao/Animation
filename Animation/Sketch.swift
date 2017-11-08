746887b36508a29caefa9bc96b660e0589fc4042
Gao Kevin <kgao@lcs-mba3111.local>
2017-11-08 11:05:26 -0500

    Set the frame-count and the conditions

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -6,6 +6,7 @@ extension Degrees {
 }
 import Foundation
 
+
 class Sketch : NSObject {
     
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
@@ -13,11 +14,10 @@ class Sketch : NSObject {
     let canvas : Canvas
     
     // Position of circle
-    var x : Int
+    var x : Degrees
     var y : Int
     var x2 : Int
     var y2 : Int
-    
     
     // This function runs once
     override init() {
@@ -28,11 +28,12 @@ class Sketch : NSObject {
         // Set starting position
         x = 0
         y = 0
-        x2 = 500
-        y2 = 500
+        x2 = 0
+        y2 = 0
+        
         
         canvas.drawShapesWithBorders = false
-    canvas.fillColor = Color.black
+        canvas.fillColor = Color.black
         canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
         
         
@@ -43,22 +44,37 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
-        x2 -= 1
+        x2 += 1
+        
+        
         
         // set the sin equation
-        y = Int(100*sin(0.02631819699*Double(x)))
+        let a : Double = 0.73
+        y = Int(100*sin(a*Double(x.toRadians()*2)))
+        
+        //set the parabola equation
+        let a2 : Double = -1/20
+        y2 = Int(a2*(Double(x2)-300)*(Double(x2)-300)+100)
+        
         
         //curves run in few times
         for k in stride(from: 0, to: 500, by: 20){
             
-        // Draw an ellipse
-            canvas.fillColor = Color(hue: k, saturation: 100, brightness: 100, alpha: 100)
-        canvas.drawEllipse(centreX: x, centreY: y+k, width: 5, height: 5)
-        canvas.drawEllipse(centreX: , centreY: , width: 5, height: 5)
-            
-            
+            if canvas.frameCount < 250 {
+                
+                
+                // Draw an ellipse
+                canvas.fillColor = Color(hue: k, saturation: 100, brightness: 100, alpha: 100)
+                canvas.drawEllipse(centreX: Int(x), centreY: y+k, width: 5, height: 5)
+                canvas.drawEllipse(centreX: canvas.width - Int(x), centreY: -y+k, width: 5, height: 5)
+            } else if canvas.frameCount >= 250 {
+                    canvas.fillColor = Color.white
+                    canvas.drawEllipse(centreX: Int(x2), centreY: y2+k, width: 5, height: 5)
+                   // canvas.drawEllipse(centreX: Int(-x2), centreY: y2+k, width: 5, height: 5)
+                
+                
+            }
             
         }
-    }
-}
+    }}
 
